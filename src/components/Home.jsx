54c662f63abd56eb41d5e64076c45761712a10c6
KartikPassricha <lowspecgamer2989@gmail.com>
2026-05-02 12:10:38 +0530

    Remove emoji from the Home component header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 function Home() {
   return (
     <section id="home" className="home">
-      <h1>Hello, I'm Kartik 😎</h1>
+      <h1>Hello, I'm Kartik </h1>
       <p>Aspiring Developer | Problem Solver | Tech Enthusiast</p>
     </section>
   );
